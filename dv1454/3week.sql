774078bcc7fda922094d761da7fb04e832036d34
codeFreak43 <artback@openmailbox.org>
2016-08-30 13:59:07 +0200

    2016-08-30 13:59:07

--- dv1454/3week.sql
+++ dv1454/3week.sql
@@ -14,7 +14,8 @@ BEGIN TRANSCATION
 INSERT INTO person (name , ssn , adress)
 VALUES("jonathan","199512290100"),("karl","199212121212"),("sven","195401040000"),("per","199909090101",),("lisa","196710109999");
 INSERT INTO adress(postalNumber,streetName)
-VALUES("39480","Kungsgatan 5")("39480","Kungsgatan 3"),("39480","Kungsgatan 4"),("39480","Kungsgatan 4"),("39480","Kungsgatan 4")
+VALUES("39480","Kungsgatan 5")("39480","Kungsgatan 3"),("39480","Kungsgatan 4"),("39480","Kungsgatan 4"),("39480","Kungsgatan 4");
+COMMIT TRANSCATION 
 
 
 
