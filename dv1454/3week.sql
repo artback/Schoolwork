544915d70d459d7d78d4bca9dac2aad9b9f935e0
codeFreak43 <artback@openmailbox.org>
2016-08-31 16:17:15 +0200

    2016-08-31 16:17:15

--- dv1454/3week.sql
+++ dv1454/3week.sql
@@ -8,14 +8,12 @@ BEGIN TRANSCATION
     CREATE TABLE adress
     (
     postalNumber integer,
-    streetName varchar(255) primary key
-     varchar(40) FOREIGN KEY,
-    CONSTRAINT 
-
+    streetName varchar(255) primary key,
+    houseowner varchar(40) FOREIGN KEY,
     )
-INSERT INTO person (name , ssn , adress)
-VALUES("jonathan",199512290100),("karl",199212121212),("sven",195401040000),("per",199909090101,),("lisa",196710109999);
-INSERT INTO adress(postalNumber,streetName,owner)
+INSERT INTO person (name , ssn )
+VALUES ("jonathan",199512290100),("karl",199212121212),("sven",195401040000),("per",199909090101),("lisa",196710109999);
+INSERT INTO adress(postalNumber,streetName,houseowner)
 VALUES(39480,"Kungsgatan 5","")(39480,"Kungsgatan 3"),(39480,"Kungsgatan 4"),(39480,"Kungsgatan 4"),(39480,"Kungsgatan 4");
 COMMIT TRANSACTION
 
