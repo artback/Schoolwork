185f3421f07fed39d181e48d1a2520b931af6e5a
codeFreak43 <artback@openmailbox.org>
2016-08-31 20:50:26 +0200

    2016-08-31 20:50:26

--- dv1454/3week.sql
+++ dv1454/3week.sql
@@ -1,5 +1,5 @@
 --sql
-BEGIN TRANSCATION trans
+BEGIN TRANSACTION
     CREATE TABLE person
     (
     ssn integer PRIMARY KEY ,
@@ -11,11 +11,11 @@ BEGIN TRANSCATION trans
     streetName varchar(255) primary key,
     houseowner integer FOREIGN KEY REFERENCES person(ssn),
     )
-INSERT INTO person (name , ssn )
+INSERT INTO person(name , ssn )
 VALUES ("jonathan",199512290100),("karl",199212121212),("sven",195401040000),("per",199909090101),("lisa",196710109999);
 INSERT INTO adress(postalNumber,streetName,houseowner)
 VALUES(39480,"Kungsgatan 5",199512290100),(39480,"Kungsgatan 3",195401040000),(39480,"Kungsgatan 4",199212121212);
-COMMIT TRANSACTION trans
+COMMIT TRANSACTION
 
 
 
