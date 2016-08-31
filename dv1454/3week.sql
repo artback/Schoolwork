e6a7d26f9299fe4b515f5a3918cf767000431ee8
codeFreak43 <artback@openmailbox.org>
2016-09-01 01:00:45 +0200

    2016-09-01 01:00:45

--- dv1454/3week.sql
+++ dv1454/3week.sql
@@ -15,7 +15,7 @@ INSERT INTO person (name,ssn)
 VALUES ('jonathan','199512290100'),('karl','199212121212'),('sven','195401040000'),('per','199909090101'),('lisa','196710109999');
 INSERT INTO adress (postalNumber,streetName,houseowner)
 VALUES (39480,'Kungsgatan 5','199512290100'),(39480,'Kungsgatan 3','195401040000'),(39480,'Kungsgatan 4','199212121212')
-,(39470,'körsbärsvägen 2','199512290100'),;
+,(39470,'körsbärsvägen 2','199512290100'),(39490,'clementinvägen');
 COMMIT TRANSACTION
 
 
