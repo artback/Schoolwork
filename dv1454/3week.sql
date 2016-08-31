604ba5f01c8a3373d7fc646cd4bfe646bbbc4d9b
codeFreak43 <artback@openmailbox.org>
2016-08-31 11:03:59 +0200

    2016-08-31 11:03:59

--- dv1454/3week.sql
+++ dv1454/3week.sql
@@ -2,13 +2,14 @@
 BEGIN TRANSCATION
     CREATE TABLE person
     (
-    ssn integer primary key,
+    ssn integer PRIMARY KEY,
     name varchar(255),
     );
     CREATE TABLE adress
     (
     postalNumber integer,
     streetName varchar(255) primary key
+    owner f
 
     )
 INSERT INTO person (name , ssn , adress)
@@ -16,7 +17,6 @@ VALUES("jonathan",199512290100),("karl",199212121212),("sven",195401040000),("pe
 INSERT INTO adress(postalNumber,streetName)
 VALUES(39480,"Kungsgatan 5")(39480,"Kungsgatan 3"),(39480,"Kungsgatan 4"),(39480,"Kungsgatan 4"),(39480,"Kungsgatan 4");
 COMMIT TRANSCATION 
-
 
 
 
