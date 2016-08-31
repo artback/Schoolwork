d046ab7f321f5a426fc4441accba11c0ab9a6b14
codeFreak43 <artback@openmailbox.org>
2016-09-01 01:06:12 +0200

    2016-09-01 01:06:12

--- dv1454/3week.sql
+++ dv1454/3week.sql
@@ -12,10 +12,11 @@ BEGIN TRANSACTION
     houseowner char(12) FOREIGN KEY REFERENCES person(ssn)
     );
 INSERT INTO person (name,ssn)
-VALUES ('jonathan','199512290100'),('karl','199212121212'),('sven','195401040000'),('per','199909090101'),('lisa','196710109999');
+VALUES ('jonathan','199512290100'),('karl','199212121212'),('sven','195401040000'),('per','199909090101'),('lisa','196710109999'),
+('stina','197012120988');
 INSERT INTO adress (postalNumber,streetName,houseowner)
 VALUES (39480,'Kungsgatan 5','199512290100'),(39480,'Kungsgatan 3','195401040000'),(39480,'Kungsgatan 4','199212121212')
-,(39470,'körsbärsvägen 2','199512290100'),(39490,'clementinvägen');
+,(39470,'körsbärsvägen 2','199512290100'),(39490,'clementinvägen','197012120988');
 COMMIT TRANSACTION
 
 
