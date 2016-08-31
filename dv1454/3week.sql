b717505165b86cfdae39e96db1fa5859e1911126
codeFreak43 <artback@openmailbox.org>
2016-09-01 00:47:40 +0200

    2016-09-01 00:47:40

--- dv1454/3week.sql
+++ dv1454/3week.sql
@@ -2,14 +2,14 @@
 BEGIN TRANSACTION
     CREATE TABLE person
     (
-    name varchar(40) NOT NULL ,
-    ssn char PRIMARY KEY ,
+    name varchar(40) ,
+    ssn char(12) PRIMARY KEY
     );
     CREATE TABLE adress
     (
     postalNumber integer NOT NULL ,
     streetName varchar(255) primary key,
-    houseowner char(12) FOREIGN KEY REFERENCES person(ssn),
+    houseowner char(12) FOREIGN KEY REFERENCES person(ssn)
     );
 INSERT INTO person (name,ssn)
 VALUES ('jonathan','199512290100'),('karl','199212121212'),('sven','195401040000'),('per','199909090101'),('lisa','196710109999');
