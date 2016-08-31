2e36ed754cea6c31bf058f64937e8b2cf907cffe
codeFreak43 <artback@openmailbox.org>
2016-08-31 15:04:03 +0200

    2016-08-31 15:04:03

--- dv1454/3week.sql
+++ dv1454/3week.sql
@@ -9,7 +9,7 @@ BEGIN TRANSCATION
     (
     postalNumber integer,
     streetName varchar(255) primary key
-    owner varchar(40) FOREIGN KEY,
+     varchar(40) FOREIGN KEY,
     CONSTRAINT 
 
     )
@@ -17,7 +17,7 @@ INSERT INTO person (name , ssn , adress)
 VALUES("jonathan",199512290100),("karl",199212121212),("sven",195401040000),("per",199909090101,),("lisa",196710109999);
 INSERT INTO adress(postalNumber,streetName,owner)
 VALUES(39480,"Kungsgatan 5","")(39480,"Kungsgatan 3"),(39480,"Kungsgatan 4"),(39480,"Kungsgatan 4"),(39480,"Kungsgatan 4");
-COMMIT TRANSCATION 
+COMMIT TRANSACTION
 
 
 
