d6a9b636f0e49b12e3664612da42086c943491f5
codeFreak43 <artback@openmailbox.org>
2016-09-20 16:06:19 +0200

    2016-09-20 16:06:19

---
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/Event.java
@@ -0,0 +1,7 @@
+package com.artback.bth.timeplanner;
+
+/**
+ * Created by freak on 9/20/16.
+ */
+public class Event {
+}
--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/Location.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/Location.java
@@ -4,4 +4,7 @@ package com.artback.bth.timeplanner;
  * Created by freak on 9/20/16.
  */
 public class Location {
+   private String name;
+   private String lat;
+   private String lon;
 }
