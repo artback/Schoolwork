4d773d6698870102b2ce3e887550c948b3545286
codeFreak43 <artback@openmailbox.org>
2016-09-21 20:20:03 +0200

    2016-09-21 20:20:03

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/AddLocationActivty.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/AddLocationActivty.java
@@ -1,8 +1,54 @@
 package com.artback.bth.timeplanner;
 
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+
 /**
  * Created by freak on 9/21/16.
  */
 public class AddLocationActivty {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_location_page);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        locationView.setLayoutManager(llm);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        locationLayoutManager= new LinearLayoutManager(this);
+        locationView.setLayoutManager(locationLayoutManager);
+        Location[] myLocationSet = new Location[10];
+        locAdapter = new locationAdapter(myLocationSet);
+        locationView.setAdapter(locAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        return super.onOptionsItemSelected(item);
+    }
 }
