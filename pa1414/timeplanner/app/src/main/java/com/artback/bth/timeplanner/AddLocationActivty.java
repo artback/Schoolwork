5106feeb70fdf942dd9fdbfe200f6dd202af935e
codeFreak43 <artback@openmailbox.org>
2016-09-21 20:14:59 +0200

    2016-09-21 20:14:59

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/AddLocationActivty.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/AddLocationActivty.java
@@ -4,4 +4,5 @@ package com.artback.bth.timeplanner;
  * Created by freak on 9/21/16.
  */
 public class AddLocationActivty {
+
 }
--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/MainActivity.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
+
 public class MainActivity extends Activity {
     private RecyclerView locationView;
     private RecyclerView.Adapter locAdapter;
