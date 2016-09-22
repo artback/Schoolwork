9fd16f5abdd29d39f877dc78744c08a7725f5ca1
codeFreak43 <artback@openmailbox.org>
2016-09-22 07:53:36 +0200

    2016-09-22 07:53:36

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/MainActivity.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends Activity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Context.start
+                
             }
         });
 
