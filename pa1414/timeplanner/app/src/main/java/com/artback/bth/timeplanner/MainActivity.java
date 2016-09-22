9ce03f05c61f8db24d8428100b6c7c32c0080a3f
codeFreak43 <artback@openmailbox.org>
2016-09-22 07:54:23 +0200

    2016-09-22 07:54:23

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/MainActivity.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/MainActivity.java
@@ -1,6 +1,7 @@
 package com.artback.bth.timeplanner;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
@@ -29,7 +31,8 @@ public class MainActivity extends Activity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                startActivity(intent);
+
             }
         });
 
