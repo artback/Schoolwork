1a1e731039bb4b6e124900c6bd1c13ffa6b78b97
codeFreak43 <artback@openmailbox.org>
2016-09-25 14:14:38 +0200

    2016-09-25 14:14:38

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/EventsFragment.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/EventsFragment.java
@@ -1,9 +1,13 @@
 package com.artback.bth.timeplanner;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
