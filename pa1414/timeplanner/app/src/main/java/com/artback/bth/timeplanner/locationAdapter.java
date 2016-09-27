00888c6023770ad96e854eb989f38445a01662c7
codeFreak43 <artback@openmailbox.org>
2016-09-27 22:36:33 +0200

    2016-09-27 22:36:33

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/locationAdapter.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/locationAdapter.java
@@ -53,7 +53,8 @@ public class locationAdapter extends RecyclerView.Adapter<locationAdapter.ViewHo
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         GeofenceLocation location = mGeofenceLocations.get(position);
-        TextView textView = holder.locationTextView
+        TextView textView = holder.locationTextView;
+        
         //holder.mView.setText(mDataset[position].getName());
     }
     // Return the size of your dataset (invoked by the layout manager)
