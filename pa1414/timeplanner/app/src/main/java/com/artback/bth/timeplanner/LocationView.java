c2bb918822bf1a7b371dc817324698b17b62e705
codeFreak43 <artback@openmailbox.org>
2016-09-27 15:49:20 +0200

    2016-09-27 15:49:20

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/LocationView.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/LocationView.java
@@ -4,9 +4,6 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 
-/**
- * Created by freak on 9/27/16.
- */
 
 public class LocationView extends View {
     public LocationView(Context context, AttributeSet attrs) {
