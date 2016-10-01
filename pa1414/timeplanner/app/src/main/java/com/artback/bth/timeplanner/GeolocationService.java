ae997e9fd520ce61bb76d41f677e8a41737768c5
codeFreak43 <artback@openmailbox.org>
2016-10-01 14:10:56 +0200

    2016-10-01 14:10:56

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/GeolocationService.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/GeolocationService.java
@@ -115,7 +115,10 @@ public class GeolocationService extends Service implements ConnectionCallbacks,
 	}
 
 	protected void startLocationUpdates() {
-		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+				!= PackageManager.PERMISSION_GRANTED &&
+				ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 			// TODO: Consider calling
 			//    ActivityCompat#requestPermissions
 			// here to request the missing permissions, and then overriding
