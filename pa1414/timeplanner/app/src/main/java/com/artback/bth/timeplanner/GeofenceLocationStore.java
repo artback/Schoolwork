4235ed1aff6ad537a11a53694d0b5137c6f08a6a
codeFreak43 <artback@openmailbox.org>
2016-10-03 12:58:58 +0200

    2016-10-03 12:58:58

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/GeofenceLocationStore.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/GeofenceLocationStore.java
@@ -23,7 +23,7 @@ public class GeofenceLocationStore {
 				100, GEOFENCE_EXPIRATION_IN_MILLISECONDS,
 				com.google.android.gms.location.Geofence.GEOFENCE_TRANSITION_DWELL
 						| com.google.android.gms.location.Geofence.GEOFENCE_TRANSITION_EXIT));
-		geofences.put("Fijutsi", new GeofenceLocation("fijutsi", 56.167982 , 15.587184,
+		geofences.put("Fijutsi", new GeofenceLocation("fijutsi", 56.18272, 15.587184,
 				100, GEOFENCE_EXPIRATION_IN_MILLISECONDS,
 				com.google.android.gms.location.Geofence.GEOFENCE_TRANSITION_DWELL
 						| com.google.android.gms.location.Geofence.GEOFENCE_TRANSITION_EXIT));
