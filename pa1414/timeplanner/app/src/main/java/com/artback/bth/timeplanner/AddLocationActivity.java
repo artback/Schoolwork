e080160866ed2e21d5e5bcaccc9eb8dab168f502
codeFreak43 <artback@openmailbox.org>
2016-10-03 16:04:17 +0200

    2016-10-03 16:04:17

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/AddLocationActivity.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/AddLocationActivity.java
@@ -7,7 +7,6 @@ public class AddLocationActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_page);
 
