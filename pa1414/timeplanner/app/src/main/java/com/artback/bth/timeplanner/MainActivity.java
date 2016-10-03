4ebc08ed065975eca6ba494484b7e5e693ed41ae
codeFreak43 <artback@openmailbox.org>
2016-10-03 16:21:54 +0200

    2016-10-03 16:21:54

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/MainActivity.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/MainActivity.java
@@ -28,7 +28,11 @@ public class MainActivity extends Activity {
         LinearLayoutManager llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         locationView.setLayoutManager(llm);
-        
+        locationView.addOnItemTouchListener(
+                new RecyclerItemClickListener(context, recyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
+                        // do whatever
+                    }
         //start geolocation
         startService(new Intent(this, GeolocationService.class));
 
