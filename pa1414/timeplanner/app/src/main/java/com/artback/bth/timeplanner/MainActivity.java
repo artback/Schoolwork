a2fa7318135945b274c80251051f3f0d1e3c4937
codeFreak43 <artback@openmailbox.org>
2016-10-03 23:12:55 +0200

    2016-10-03 23:12:55

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/MainActivity.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/MainActivity.java
@@ -30,7 +30,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
-        checkPermissons();
         init();
     }
     private int checkPermissons(){
