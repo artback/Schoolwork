5a70a17902f9dd0430a90ca56818385284160b88
codeFreak43 <artback@openmailbox.org>
2016-10-03 13:49:38 +0200

    2016-10-03 13:49:38

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/GeofenceLocationStore.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/GeofenceLocationStore.java
@@ -27,6 +27,7 @@ public class GeofenceLocationStore {
 				100, GEOFENCE_EXPIRATION_IN_MILLISECONDS,
 				com.google.android.gms.location.Geofence.GEOFENCE_TRANSITION_DWELL
 						| com.google.android.gms.location.Geofence.GEOFENCE_TRANSITION_EXIT));
+		
 	}
 
 	public HashMap<String, GeofenceLocation> getSimpleGeofences() {
