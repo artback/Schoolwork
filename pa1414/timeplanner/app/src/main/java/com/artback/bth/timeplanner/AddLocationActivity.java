ffba4f0b795d2522b8f2037ea1d36eac7abb6029
codeFreak43 <artback@openmailbox.org>
2016-10-03 16:19:20 +0200

    2016-10-03 16:19:20

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/AddLocationActivity.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/AddLocationActivity.java
@@ -2,6 +2,7 @@ package com.artback.bth.timeplanner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddLocationActivity extends Activity{
 
@@ -10,6 +11,9 @@ public class AddLocationActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_page);
 
+
+    }
+    public void onclick(View view){
 
     }
 
