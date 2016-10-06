6c3a946a45f7bdc908510ce974d426db1a9d4365
codeFreak43 <artback@openmailbox.org>
2016-10-06 12:26:41 +0200

    2016-10-06 12:26:41

--- dv1454/TravelPlanner/src/gui.java
+++ dv1454/TravelPlanner/src/gui.java
@@ -7,7 +7,6 @@ import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.awt.event.ActionEvent;
-import com.Toa
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
