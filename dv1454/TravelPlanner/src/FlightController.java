35f39c753446e6e70fd7fad6a865e3b53d25a558
codeFreak43 <artback@openmailbox.org>
2016-10-07 13:03:14 +0200

    2016-10-07 13:03:14

--- dv1454/TravelPlanner/src/FlightController.java
+++ dv1454/TravelPlanner/src/FlightController.java
@@ -62,7 +62,7 @@ public class FlightController {
 	
     public String[][] getAllFlights() {
         String[][] flights = new String[0][];
-        flights = this.dbGetFlights("SELECT * FROM flight");
+        flights = this.dbGetFlights("SELECT * FROM flight INNER JOIN Travel");
         return flights;
     }
     
