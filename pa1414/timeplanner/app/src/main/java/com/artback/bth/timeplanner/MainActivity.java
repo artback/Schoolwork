e0821b30a96308d68cc4bda4a21ce3111bb043f1
codeFreak43 <artback@openmailbox.org>
2016-10-07 10:25:44 +0200

    2016-10-07 10:25:44

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/MainActivity.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/MainActivity.java
@@ -88,8 +88,6 @@ public class MainActivity extends Activity {
                     }
                 })
         );
-
-
         locationView.setAdapter(locAdapter);
     }
     public void openaddpage(View view){
