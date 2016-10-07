55f72f5448ac05e2a14df0b8b05b283d8e563cfd
codeFreak43 <artback@openmailbox.org>
2016-10-07 16:39:15 +0200

    2016-10-07 16:39:15

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/locationAdapter.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/locationAdapter.java
@@ -1,7 +1,5 @@
 package com.artback.bth.timeplanner;
-import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
