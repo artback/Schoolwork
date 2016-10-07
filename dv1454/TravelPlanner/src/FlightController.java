32341d4ee1e910bb35cd5f90bda98fa1ea6e5fb0
codeFreak43 <artback@openmailbox.org>
2016-10-07 14:17:02 +0200

    2016-10-07 14:17:02

--- dv1454/TravelPlanner/src/FlightController.java
+++ dv1454/TravelPlanner/src/FlightController.java
@@ -1,9 +1,7 @@
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class FlightController {
     public FlightController() {
@@ -11,7 +9,6 @@ public class FlightController {
     
 
     private Integer[] dbGet(String SQL)  {
-        Connection connection = null;
         Integer flight[] = new Integer[2];
 
             ResultSet rs = db.query(SQL);
@@ -62,72 +59,20 @@ public class FlightController {
 	
     public String[][] getAllFlights() {
         String[][] flights = new String[0][];
-        flights = this.dbGetFlights("SELECT  FROM Flight INNER JOIN Travel on Flight.TravelID=Travel.TravelID");
+        flights = this.dbGetFlights("SELECT * FROM Flight INNER JOIN Travel on Flight.TravelID=Travel.TravelID");
         return flights;
     }
     
     public String[][] getFlights(String origin, String destination, String date) {
         String[][] flights = new String[0][];
-        try {
             flights = this.dbGetFlights(
                 "SELECT * FROM flights WHERE origin = '" +
                 origin + "' AND destination = '" + destination +
                 "' AND departure_date = '" + date + "'"
             );
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
         return flights;
     }
 
-    public boolean addFlight(String origin, String destination, String deptDate,
-        String deptTime, String travelTime, int price, int seats) {
-        return this.addToDatabase(
-            origin, destination,deptDate, deptTime, travelTime, price, seats
-        );
-    }
 
-    public boolean removeFlight(int id){
-        return this.deleteFromDatabase(id);
-    }
 
-    public boolean updateFlight(int id,String origin, String destination,
-        String deptDate, String deptTime, String travelTime, int price, int seats) {
-        return this.updateDatabase(
-            id,origin, destination,deptDate, deptTime, travelTime, price, seats
-        );
-    }
-
-    private boolean addToDatabase(String origin, String destination,
-        String deptDate, String deptTime, String travelTime, int price, int seats) {
-        return this.dbInsert(
-            "INSERT INTO flights VALUES(" + null + ",'" +
-            origin + "', '" +
-            destination + "', '" +
-            deptDate + "', '" +
-            deptTime + "', '" +
-            travelTime + "', " +
-            price +", " +
-            seats + ")"
-        );
-    }
-	
-    private boolean deleteFromDatabase(int id){
-        return this.dbDelete("DELETE FROM flights WHERE flight_id = "+ id);
-    }
-
-    private boolean updateDatabase(int id, String origin, String destination,
-        String deptDate, String deptTime, String travelTime, int price, int seats){
-        return this.dbInsert(
-            "UPDATE flights SET " +
-            "origin = '" + origin + 
-            "', destination = '" + destination + 
-            "', departure_date = '" + deptDate +
-            "', departure_time = '" + deptTime +
-            "', travel_time = '" + travelTime +
-            "', price = " + price +
-            ",  nr_of_seats = " + seats + 
-            " WHERE flight_id = " + id
-        );
-    }
 }
