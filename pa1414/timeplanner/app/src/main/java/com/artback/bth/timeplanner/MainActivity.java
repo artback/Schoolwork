01af8e91018db34e0b7a4925d1d515b508beac49
codeFreak43 <artback@openmailbox.org>
2016-10-07 15:02:31 +0200

    2016-10-07 15:02:31

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/MainActivity.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/MainActivity.java
@@ -3,7 +3,6 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
