62a99a900dcc262524739015e0ecd302b0a872d6
codeFreak43 <artback@openmailbox.org>
2016-10-07 15:38:47 +0200

    2016-10-07 15:38:47

--- pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/locationAdapter.java
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/locationAdapter.java
@@ -16,7 +16,8 @@ public class locationAdapter extends RecyclerView.Adapter<locationAdapter.ViewHo
         // Provide a reference to the views for each data item
         // Complex data items may need more than one view per item, and
         // you provide access to all the views for a data item in a view holder
-        public static class ViewHolder extends RecyclerView.ViewHolder {
+        public static class ViewHolder extends RecyclerView.ViewHolder
+                {
             // each data item is just a string in this case
             private TextView locationTextView;
             private TextView timeTextView;
