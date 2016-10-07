23893a00c990b06ea3ff233598f1f6f68d9f4df3
codeFreak43 <artback@openmailbox.org>
2016-10-07 09:29:07 +0200

    2016-10-07 09:29:07

---
+++ pa1414/timeplanner/app/src/main/java/com/artback/bth/timeplanner/CalenderActivty.java
@@ -0,0 +1,24 @@
+package com.artback.bth.timeplanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by freak on 10/7/16.
+ */
+
+public class CalenderActivty extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calender_page);
+
+    }
+    public void onclick(View view){
+
+    }
+
+}
+
