a30e8b46c9c5881d91c176e113dcc4c5666f7e52
codeFreak43 <artback@openmailbox.org>
2016-10-10 11:38:20 +0200

    2016-10-10 11:38:20

--- dv1454/TravelPlanner/src/AccountController.java
+++ dv1454/TravelPlanner/src/AccountController.java
@@ -8,18 +8,13 @@ public class AccountController {
     private void addUserToDB(String firstName, String lastName, String Adress,
                                 String Email,String PhoneNr) {
 
-            ResultSet rs = db.query("SELECT ");
+            ResultSet rs = db.query("INSERT INTO Customer VALUES(" + null + ", " +
+                            "'" + firstName + "', " +
+                            "'" + lastName + "', " +
+                            "'" + firstName + "', " +
+                            "'" + lastName + "' )");
+;
 
-            //IF NO SUCH USER EXISTS ADD THE USER
-            if( !rs.isBeforeFirst() ) {
-                statement.executeUpdate(
-                    "INSERT INTO Customer VALUES(" + null + ", " +
-                    "'" + this.user + "', " +
-                    "'" + this.password + "', " +
-                    "'" + firstName + "', " +
-                    "'" + lastName + "' )"
-                ); 
-            }
     }
     
 }
